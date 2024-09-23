a340bb5c485b11f5b6ac0f30afb4e326c1fbb3a8
paulo <paulo.m.campos6601@gmail.com>
2024-09-23 10:39:22 -0300

    add: start project

---
+++ fcmanagement/src/main/java/com/example/fcmanagement/Server.java
@@ -0,0 +1,5 @@
+package com.example.fcmanagement;
+
+public class Server {
+
+}
---
+++ fcmanagement/src/test/java/com/example/fcmanagement/FcmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
