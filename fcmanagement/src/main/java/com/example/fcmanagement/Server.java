d85bc66d435ca9610383804c8a09081538f8ab21
paulo <paulo.m.campos6601@gmail.com>
2024-09-23 15:39:18 -0300

    add: start building the client and server

--- fcmanagement/src/main/java/com/example/fcmanagement/Server.java
+++ fcmanagement/src/main/java/com/example/fcmanagement/Server.java
@@ -1,5 +1,38 @@
 package com.example.fcmanagement;
 
+import java.util.Scanner;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+
 public class Server {
 
+    private final static String QUEUE = "messages";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost"); // change to RabbitMQ server address
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE, false, false, false, null);
+
+            System.out.println("Server started. Type the message: ");
+
+            Scanner in = new Scanner(System.in);
+            String message;
+
+            while (true) {
+                System.out.println("Type the message to the client (or type exit to leave)...");
+                message = in.nextLine();
+
+                if ("exit".equalsIgnoreCase(message)) {
+                    System.out.println("Server Finished");
+                    break;
+                }
+            }
+
+            in.close();
+        }
+    }
 }
--- fcmanagementPy/src/client.py
+++ fcmanagementPy/src/client.py
@@ -1,7 +1,18 @@
 import pika
 
-url = "amqps://ahpmdfzr:peFA7z9DlvjLU6N_IgUGg2U6g_r9xo-f@prawn.rmq.cloudamqp.com/ahpmdfzr"
-params = pika.URLParameters(url)
+def callback(ch, method, properties, body):
+    print(f"Mensagem recebida: {body.decode()}")
 
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
+def recieve():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  # Use o IP/host do RabbitMQ
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='messages')
+    
+    channel.basic_consume(queue='messages', on_message_callback=callback, auto_ack=True)
+    
+    print("Awaiting messages... to leave, press CTRL+C")
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    recieve()
